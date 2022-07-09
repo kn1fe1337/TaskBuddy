1c32b2952dc41f2eac4f592e22011aa665c046e0
kn1fe1337 <104734183+kn1fe1337@users.noreply.github.com>
2022-07-10 02:11:31 +0300

    Add files via upload

---
+++ taskinator.py
@@ -0,0 +1,60 @@
+tasks = [] 
+
+quitt = 0 
+
+  
+
+print("Welcome to Taskinator") 
+
+print("") 
+
+print("⊂(◉‿◉)つ") 
+
+print("") 
+
+print("1. Add new task") 
+
+print("") 
+
+print("2. View your tasks") 
+
+print("") 
+
+print("Enter command(1 or 2):") 
+
+print("") 
+
+menu = input("Enter:") 
+
+if menu == "1" : 
+
+    while quitt < 1: 
+
+        print("") 
+
+        new_task = input("Enter task name:") 
+
+        tasks.append(new_task) 
+
+        if new_task == "0": 
+            menu = input("Enter:")
+            break 
+            
+
+if menu == "2" : 
+
+    for x in tasks: 
+
+        print("") 
+
+        print(x) 
+
+        with open(r'C:\Users\gotin\Desktop\Projects\python\taskbuddy\tasks.txt', 'w') as f: 
+
+           for x in tasks: 
+
+                f.write("%s\n" % "**********************************Task*****************************************") 
+
+                f.write("%s\n" % x) 
+
+                 
